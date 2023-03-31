4ede45e1b00e5bf1a29b097c2a306f3582ca0766
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-03-31 17:49:59 +0600

    font awesome added

---
+++ src/Componets/Blog/Blog.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BlogContent from '../BlogContent/BlogContent';
+
+const Blog = () => {
+    return (
+       <div className='grid'>
+     <BlogContent/>
+     
+       </div>
+    );
+};
+
+export default Blog;
---
+++ src/Componets/BlogContent/BlogContent.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons'
+
+const BlogContent = () => {
+    return (
+        <div className='m-4'>
+            <img src="" alt="" className='w-full ' />
+            <div className='flex  justify-between'>
+              <div>
+              <img src="" alt="" className='rounded-full w-10 h-10' />
+                <div><p className='text-3xl'>Mr.Raju</p>
+                    <br />
+                  <p className='text-sm text-slate-600'>Mar 14 (4 day ago)</p>  
+                </div>
+                </div>
+                <div>
+                <p className='text-sm text-slate-600 '>5-min read
+                <FontAwesomeIcon icon={faBookmark}  className="mx-5"/>
+                </p>
+                </div>
+              
+            </div>
+            <h4 className='text-5xl'>hello</h4>
+            <p>#tourist #tour</p>
+            <a className='text-violet-800 underline underline-offset-4'>Mark as read</a>
+        </div>
+    );
+};
+
+export default BlogContent;
